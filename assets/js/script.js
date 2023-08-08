69e0ff48fa552ef24bb3f532d2ce5ed09fd2f12a
cbstange <>
2023-08-08 17:04:39 +0000

    fix css properties for choice-container class

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ let score = 0;
 let currentQuestion = {};
 let questionCounter = 0;
 let availableQuestions = [];
-let acceptingAnswers = true;
 
 // QUESTIONS ARRAY
 
@@ -13,25 +12,57 @@ let questions = [
     {
         question: "Test question placeholder?",
         choice1: "sample one",
-        choice2: "sample two",
-        choice3: "sample three",
+        choice2: "sampdddddddle two",
+        choice3: "sa;alskdjf",
         choice4: "sample four",
         answer: 1
     },
     {
-        question: "Test question placeholder?",
-        choice1: "sample one",
-        choice2: "sample two",
-        choice3: "sample three",
+        question: "Random question",
+        choice1: "samsergfdple one",
+        choice2: "sampddddle two",
+        choice3: "samddwetewple three",
         choice4: "sample four",
         answer: 2
     },
     {
-        question: "Test question placeholder?",
-        choice1: "sample one",
+        question: "another random",
+        choice1: "samplsergfde one",
         choice2: "sample two",
         choice3: "sample three",
         choice4: "sample four",
         answer: 3
     }
-]
+];
+
+gameStart = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+};
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        const userChoice = e.target;
+        const selectedAnswer = userChoice.dataset["number"];
+
+        getNewQuestion();
+    });
+});
+
+gameStart();
