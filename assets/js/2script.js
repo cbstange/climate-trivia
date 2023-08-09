cf5d8a3e72a7e06627c244de594d6a7b4166f876
cbstange <>
2023-08-09 21:05:53 +0000

    add pseudo hover class to buttons

---
+++ assets/js/2script.js
@@ -0,0 +1,29 @@
+const questions = [
+  {
+    question: " which is larger?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: " which is largest desert?",
+    answers: [
+      { text: "kalh", correct: false },
+      { text: "gobi ", correct: false },
+      { text: "sahara", correct: false },
+      { text: "north pole", correct: true },
+    ],
+  },
+  {
+    question: " which is smallest continent?",
+    answers: [
+      { text: "asia", correct: false },
+      { text: "austria", correct: true },
+      { text: "africa", correct: false },
+      { text: "arctoc", correct: false },
+    ],
+  },
+];
