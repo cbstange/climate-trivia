bb9e87fb26ccf5cae1f572e839594622f580a78d
cbstange <>
2023-08-10 06:45:55 +0000

    add final objects to question array

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,21 +11,82 @@ const questions = [
         ],
     },
     {
-        question: " which is largest desert?",
+        question: " Since first recording global temperatures in 1880, how many degrees celcius has earth warmed?",
         answers: [
-            { text: "kalh", correct: false },
-            { text: "gobi ", correct: false },
-            { text: "sahara", correct: false },
-            { text: "north pole", correct: true },
+            { text: "5", correct: false },
+            { text: "1.1 ", correct: true },
+            { text: "Temperature has not risen.", correct: false },
+            { text: "10", correct: false },
         ],
     },
     {
-        question: " which is smallest continent?",
+        question: " Which is the single largest cause of deforestation?",
         answers: [
-            { text: "asia", correct: false },
-            { text: "austria", correct: true },
-            { text: "africa", correct: false },
-            { text: "arctoc", correct: false },
+            { text: "Paper products", correct: false },
+            { text: "Palm Oil production ", correct: false },
+            { text: "Urbanization", correct: false },
+            { text: "Meat production", correct: true },
+        ],
+    },
+    {
+        question: " What are some of the direct effects of increased global temperatures?",
+        answers: [
+            { text: "Rising sea levels", correct: false },
+            { text: "Extreme and more frequent hurricanes ", correct: false },
+            { text: "Increased wildfires", correct: false },
+            { text: "All of the above.", correct: true },
+        ],
+    },
+    {
+        question: " Approximately how many centimeters has the sea level risen since 1880?",
+        answers: [
+            { text: "50cm", correct: false },
+            { text: "10cm ", correct: false },
+            { text: "22cm", correct: true },
+            { text: "Level unchanged", correct: false },
+        ],
+    },
+    {
+        question: " What is the leading cause of ocean pollution?",
+        answers: [
+            { text: "Commercial fishing", correct: true },
+            { text: "Single-use plastics ", correct: false },
+            { text: "Textile waste", correct: false },
+            { text: "Oil refineries", correct: false },
+        ],
+    },
+    {
+        question: " Since 1990, global forests has shrunk by how many hectares?",
+        answers: [
+            { text: "200 million", correct: false },
+            { text: "Forrest size has increased ", correct: false },
+            { text: "900 thousand", correct: false },
+            { text: "80 million", correct: true },
+        ],
+    }, {
+        question: " Which of the following countries emit the most carbon dioxide??",
+        answers: [
+            { text: "USA", correct: false },
+            { text: "Russia ", correct: false },
+            { text: "China", correct: true },
+            { text: "India", correct: false },
+        ],
+    }, {
+        question: " What percentage of the global greenhouse gas emissions does the transportation sector emit?",
+        answers: [
+            { text: "17%", correct: true },
+            { text: "70% ", correct: false },
+            { text: "1%", correct: false },
+            { text: "10%", correct: false },
+        ],
+    },
+    {
+        question: " How does the fashion industry contribute to environmental degradation?",
+        answers: [
+            { text: "Deforestation", correct: false },
+            { text: "Loss of biodiversity", correct: false },
+            { text: "Soil degradation", correct: false },
+            { text: "All of the above.", correct: true },
         ],
     },
 ];
